64d11182c405cb6dbb5f5f83f604662d42591b5d
leiyang0321 <leiyang1@student.ubc.ca>
2025-12-15 14:28:29 -0500

    add tests for check_balance()

--- tests/testthat/test-check_balance.R
+++ tests/testthat/test-check_balance.R
@@ -1,3 +1,33 @@
-test_that("multiplication works", {
-  # expect_equal(function output, expected output)
+test_that("treatment counts have length 2", {
+  bal <- check_balance(data = lalonde,
+                       treatment = "treat",
+                       ref = 0,
+                       num = c("age", "educ"),
+                       cat = c("race", "married"),
+                       round = 3)
+  expect_equal(length(bal$treatment_counts), 2)
+})
+
+test_that("treatment distribution is consistent with the counts", {
+  bal <- check_balance(data = lalonde,
+                       treatment = "treat",
+                       ref = 0,
+                       num = c("age", "educ"),
+                       cat = c("race", "married"),
+                       round = 3)
+  expect_equal(bal$treatment_distribution,
+               bal$treatment_counts/sum(bal$treatment_counts))
+})
+
+test_that("balance result table has the correct dimensions", {
+  num <- c("age", "educ")
+  cat <- c("race", "married")
+  bal <- check_balance(data = lalonde,
+                       treatment = "treat",
+                       ref = 0,
+                       num = num,
+                       cat = cat,
+                       round = 3)
+  expect_equal(dim(bal$balance_results),
+               c(length(num)+length(cat), 10))
 })
